b5c067c8fdb41f49a699834650afaae322bb57eb
EvgeniyTeplov <vodolei2@gmail.com>
2020-03-13 00:14:37 +0500

    Максимум из 3х чисел [#215796]

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -2,16 +2,18 @@ package ru.job4j.condition;
 
 public class MultiMax {
     public int max (int first, int second, int third) {
-        int result;
-        if (first > second && first > third) {
-            result = first;
-            }else if(second > first && second > third) {
-            result = second;
-                }else{
-            result = third;
-        }
 
-        return result;
-
+        //int result;
+        return (first > second) ? ((first > third) ? first : second) : ((second > third) ? second : third);
+        /*
+         if (first > second && first > third) {
+           result = first;
+              }else if(second > first && second > third) {
+              result = second;
+                  }else{
+              result = third;
+        */
     }
-}
+     //   return result;
+    }
+
--- chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
@@ -8,7 +8,25 @@ public class MultiMaxTest {
     @Test
     public void whenSecondMax() {
         MultiMax check = new MultiMax();
-        int result = check.max(1,4,2);
-        assertThat(result, is(4));
+        int result = check.max(10,4,2);
+        assertThat(result, is(10));
+    }
+    @Test
+    public void whenSecondMax1() {
+        MultiMax check = new MultiMax();
+        int result = check.max(99,5,101);
+        assertThat(result, is(101));
+    }
+    @Test
+    public void whenSecondMax2() {
+        MultiMax check = new MultiMax();
+        int result = check.max(2,4,99);
+        assertThat(result, is(99));
+    }
+    @Test
+    public void whenSecondMax3() {
+        MultiMax check = new MultiMax();
+        int result = check.max(6,6,6);
+        assertThat(result, is(6));
     }
 }
