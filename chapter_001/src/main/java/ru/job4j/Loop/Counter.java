1d81f7097aa5c01fb5f7d13966e26619095e1858
EvgeniyTeplov <vodolei2@gmail.com>
2020-04-06 00:41:42 +0500

    Подсчет суммы чётных чисел в диапазоне[#215865]

---
+++ chapter_001/src/main/java/ru/job4j/Loop/Counter.java
@@ -0,0 +1,23 @@
+package ru.job4j.Loop;
+
+public class Counter {
+    public static int sum(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                sum = sum + i;
+            }
+        }
+        return sum;
+    }
+
+
+
+}
