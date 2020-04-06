cb560b785d8efbf613fd4b6786872d4739c5d757
EvgeniyTeplov <vodolei2@gmail.com>
2020-04-07 00:41:45 +0500

    Отладка программы в IDEA[#215794]

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -3,17 +3,14 @@ package ru.job4j.condition;
 public class MultiMax {
     public int max (int first, int second, int third) {
 
-        //int result;
-        return (first > second) ? ((first > third) ? first : second) : ((second > third) ? second : third);
-        /*
-         if (first > second && first > third) {
-           result = first;
-              }else if(second > first && second > third) {
-              result = second;
-                  }else{
-              result = third;
-        */
+
+        int resullt = first > second ? first : second;
+
+        resullt = resullt > third ? resullt : third;
+
+        return resullt;
+
     }
-     //   return result;
+
     }
 
